9c871bfd4f68cf1f94d7a8eca01b0649d6435481
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-17 19:37:26 -0400

    Implement grammar synthesis in synthesize_grammar function and update relocate.json with additional tarball entries

--- cli/pre_experiments.py
+++ cli/pre_experiments.py
@@ -2,11 +2,36 @@ import os
 import subprocess
 from datetime import datetime
 import sys
+import shutil
 import click
 from common import PROJECT_ROOT, CLI_DIR, USER
 
 def synthesize_grammar(benchmark):
-    pass
+    inputs_dir = os.path.join(PROJECT_ROOT, "evaluation", "gramgen", benchmark, "inputs")
+    GLADE_DIR = os.path.join("/", USER, "glade")
+    target_dir = os.path.join(GLADE_DIR, "inputs")
+    shutil.copytree(inputs_dir, target_dir, dirs_exist_ok=True)
+    GLADE_ORACLE_DIR = os.path.join(PROJECT_ROOT, "evaluation", "glade_oracle")
+
+    match benchmark:
+        case "xml":
+            oracle_cmd = f"{os.path.join(GLADE_ORACLE_DIR, "xml")} {{/}}"
+        case "re2":
+            oracle_cmd = f"{os.path.join(GLADE_ORACLE_DIR, 're2_fuzzer')} {{/}}"
+        case "sqlite3":
+            oracle_cmd = f"{os.path.join(GLADE_ORACLE_DIR, 'sqlite3_parser')} {{/}}"
+        case "json":
+            oracle_cmd = f"{os.path.join(GLADE_ORACLE_DIR, 'jsoncpp_fuzzer')} {{/}}"
+        case "cpython3":
+            oracle_cmd = f"python {os.path.join(GLADE_ORACLE_DIR, 'pyparser.py')} {{/}}"
+        case "librsvg":
+            oracle_cmd = f"{os.path.join(GLADE_ORACLE_DIR, 'render_document')}"
+        case "cvc5":
+            oracle_cmd = f"python {os.path.join(GLADE_ORACLE_DIR, 'cvc5_parser.py')} {{/}}"
+
+    learn_cmd = ["./gradlew", "run", "--args", f"learn -rd 100 -l 0-100 '{oracle_cmd}'"]
+    click.echo(f"Running GLADE to mine grammar for {benchmark} (may needs several hours)...")
+    subprocess.run(learn_cmd, check=True, env=os.environ.copy(), cwd=GLADE_DIR, user=USER, shell=True)
 
 def synthesize_fuzzer(target, benchmark, *, tgi_waiting=600, debug=False):
     match target:
