5bce6c466293b08617b206b9060085056b447e52
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-18 16:11:42 -0400

    Update subprocess.run to use shell execution for command string

--- evaluation/workdir/batchrun.py
+++ evaluation/workdir/batchrun.py
@@ -163,7 +163,7 @@ def main(config_file, log_level):
         run_cmd += ['-j', '28', '-t', TIME_LIMIT, '-z', method, '-c', CHECKPOINT ,  benchmark]
         if in_race:
             run_cmd += ['--race-mode']
-        subprocess.run(run_cmd, check=True, stderr=sys.stderr, stdout=sys.stdout, env=os.environ.copy())
+        subprocess.run(" ".join(run_cmd), check=True, stderr=sys.stderr, stdout=sys.stdout, shell=True)
         end_time = datetime.now()
         logger.info(f'Finish {benchmark}_{method} [{i}/{len(combinations)}] at {end_time}')
 
