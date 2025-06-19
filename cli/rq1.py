91664ee3bd0ba76b1868f4d59a593a4c26e7a740
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-19 14:17:31 -0400

    Update input directory structure in rq1_afl_run to include benchmark name

--- cli/rq1.py
+++ cli/rq1.py
@@ -326,7 +326,7 @@ def rq1_afl_run(fuzzers, benchmarks, repeat: int, debug: bool=False) -> list[tup
         cmd = [
             "python", EXPERIMENT_SCRIPT,
             "-t", "86400",
-            "-i", input_dir,
+            "-i", os.path.join(input_dir, f"{benchmark}_{subname}"),
             "-o", output_dir + r"/%d/",
             "-j", "25",
             "-r", str(repeat),
