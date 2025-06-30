7678ccfa08cda2ed8ce2247901a3209dd6f8bd6e
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-30 15:33:35 -0400

    Refactor cmin function to streamline process directory handling and file listing

--- cli/minimize.py
+++ cli/minimize.py
@@ -78,12 +78,12 @@ def cmin(fuzzers, benchmarks, tmpdir):
         for benchmark in ALL_BENCHMARKS:
             if benchmark not in benchmarks or fuzzer not in [FUZZER_MAPPING[fuzzer] for fuzzer in fuzzers]:
                 exclude.append(f"{benchmark}_{fuzzer}")
+    process_dir = os.path.join(tmpdir, "process")
+    all_prcs_files = [f for f in os.listdir(process_dir) if f.endswith(".tar.zst")]
+    intermediate_dir = os.path.join(tmpdir, "intermediate")
     for benchmark in benchmarks:
         for fuzzer_raw in fuzzers:
             fuzzer = FUZZER_MAPPING[fuzzer_raw]
-            process_dir = os.path.join(tmpdir, "process", f"{benchmark}_{fuzzer}")
-            all_prcs_files = [f for f in os.listdir(process_dir) if f.endswith(".tar.zst")]
-            intermediate_dir = os.path.join(tmpdir, "intermediate")
             raw_ori = os.path.join(PROJECT_ROOT, "extradata", "seeds", "raw")
             if f"{benchmark}_{fuzzer}" in ["re2_islearn", "jsoncpp_islearn"]:
                 continue
