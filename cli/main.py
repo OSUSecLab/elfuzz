3338f919e92de6986c9e2823b541e44140c179e9
Chuyang Chen <chuyangchen2018@outlook.com>
2025-06-30 12:38:39 -0400

    Fix help message formatting for fix-version option in download command

--- cli/main.py
+++ cli/main.py
@@ -196,8 +196,7 @@ def synthesize_on_cluster():
 @click.option("--only-relocate", "-s", is_flag=True, default=False,
               help="Only relocate the files from the cache directory to the data directory without downloading and unziping them again.")
 @click.option("--fix-version", type=int, default=-1, show_default=True,
-              help="Fix the version of the data files to download. If set to -1, it will download the latest version. \
-              If set to a positive integer, it will download the specified version.")
+              help="Fix the version of the data files to download. If set to -1, it will download the latest version. If set to a positive integer, it will download the specified version.")
 def download(ignore_cache: bool, only_relocate: bool, fix_version: int):
     click.echo("Downloading data files needed by the experiments...")
     download_mod.download_data(ignore_cache=ignore_cache, only_relocate=only_relocate, fix_version=fix_version)
