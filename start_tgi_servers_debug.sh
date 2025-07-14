0ee2d9309a4c0fc4e76aa3b368ecf4236e776b04
Chuyang Chen <chuyangchen2018@outlook.com>
2025-07-14 11:12:30 -0400

    Refactor max-input-length and max-batch-prefill-tokens in start_tgi_servers_debug.sh to use a variable for better maintainability and consistency in resource allocation during server startup.

--- start_tgi_servers_debug.sh
+++ start_tgi_servers_debug.sh
@@ -14,9 +14,10 @@ token=$(cat ${HOME}/.config/huggingface/token)
 #    --max-total-tokens 8192 --max-input-length 8000 --max-batch-prefill-tokens 8000
 
 # Code Llama: 8193, GPUs 2,3
+INPUT_TOKEN=2048
 port=8192
 model=microsoft/phi-2
 docker run --rm --gpus all -e HUGGING_FACE_HUB_TOKEN=$token --shm-size 1g \
     -p ${port}:80 -v $volume:/data ghcr.io/huggingface/text-generation-inference:1.4.5 \
     --model-id $model --trust-remote-code --dtype bfloat16 \
-    --max-total-tokens 8192 --max-input-length 4096 --max-batch-prefill-tokens 4096 --enable-cuda-graphs
+    --max-total-tokens 8192 --max-input-length $INPUT_TOKEN --max-batch-prefill-tokens $INPUT_TOKEN --enable-cuda-graphs
