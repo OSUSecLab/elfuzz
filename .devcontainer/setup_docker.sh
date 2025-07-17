5e9af6a3de010c88dacb8d543c4e0e00d9455276
Chuyang Chen <chuyangchen2018@outlook.com>
2025-07-17 10:56:51 -0400

    Add sudo to groupadd and usermod commands in setup_docker.sh for proper permissions

--- .devcontainer/setup_docker.sh
+++ .devcontainer/setup_docker.sh
@@ -7,8 +7,8 @@ DOCKER_GROUP=docker
 
 if [ -S ${DOCKER_SOCKET} ]; then
     DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-    groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
-    usermod -aG ${DOCKER_GROUP} appuser
+    sudo groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
+    sudo usermod -aG ${DOCKER_GROUP} appuser
 fi
 
 sudo chown appuser:appuser /tmp/host/fuzzdata
